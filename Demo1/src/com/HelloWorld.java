c48f8e92903f54d6c45445eea4836a03b9ef1e5d
gino <496884090@qq.com>
2017-10-11 21:33:00 +0800

    add helloworld

---
+++ Demo1/src/com/HelloWorld.java
@@ -0,0 +1,9 @@
+package com;
+
+public class HelloWorld{
+    public static void main(String [] args){
+        System.out.println("Hello World!");
+        System.out.println("Hello github!");
+        System.out.println("Hello git!");
+    }
+}
